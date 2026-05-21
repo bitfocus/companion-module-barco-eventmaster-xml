fff4e254f7b0ff58aa375ed457827ffcbd0fa354
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2026-05-21 09:02:45 +0200

    add test tone command

--- index.js
+++ index.js
@@ -1,148 +1,184 @@
 const { InstanceBase, runEntrypoint, TCPHelper, InstanceStatus } = require('@companion-module/base')
 
 class EventmasterXmlInstance extends InstanceBase {
-  constructor(internal) {
-    super(internal)
-    this.tcp = null
-  }
+	constructor(internal) {
+		super(internal)
+		this.tcp = null
+	}
 
-  async init(config) {
-    this.config = config
-    this.updateStatus(InstanceStatus.Connecting)
-    this.initTCP()
-    this.initActions()
-  }
+	async init(config) {
+		this.config = config
+		this.updateStatus(InstanceStatus.Connecting)
+		this.initTCP()
+		this.initActions()
+	}
 
-  async destroy() {
-    if (this.tcp) {
-      this.tcp.destroy()
-      this.tcp = null
-    }
-  }
+	async destroy() {
+		if (this.tcp) {
+			this.tcp.destroy()
+			this.tcp = null
+		}
+	}
 
-  async configUpdated(config) {
-    this.config = config
-    this.initTCP()
-  }
+	async configUpdated(config) {
+		this.config = config
+		this.initTCP()
+	}
 
-  getConfigFields() {
-    return [
-      {
-        type: 'textinput',
-        id: 'host',
-        label: 'Target IP',
-        width: 6,
-        default: '',
-      },
-      {
-        type: 'textinput',
-        id: 'macAddress',
-        label: 'Target MAC Address',
-        width: 6,
-        default: '',
-      },
-    ]
-  }
+	getConfigFields() {
+		return [
+			{
+				type: 'textinput',
+				id: 'host',
+				label: 'Target IP',
+				width: 6,
+				default: '',
+			},
+			{
+				type: 'textinput',
+				id: 'macAddress',
+				label: 'Target MAC Address',
+				width: 6,
+				default: '',
+			},
+		]
+	}
 
-  initTCP() {
-    if (this.tcp) {
-      this.tcp.destroy()
-      this.tcp = null
-    }
+	initTCP() {
+		if (this.tcp) {
+			this.tcp.destroy()
+			this.tcp = null
+		}
 
-    if (this.config.host) {
-      this.tcp = new TCPHelper(this.config.host, 9876)
+		if (this.config.host) {
+			this.tcp = new TCPHelper(this.config.host, 9876)
 
-      this.tcp.on('status_change', (status, message) => {
-        this.updateStatus(status, message)
-      })
+			this.tcp.on('status_change', (status, message) => {
+				this.updateStatus(status, message)
+			})
 
-      this.tcp.on('error', (err) => {
-        this.log('error', `TCP Error: ${err.message}`)
-      })
-    }
-  }
+			this.tcp.on('error', (err) => {
+				this.log('error', `TCP Error: ${err.message}`)
+			})
+		}
+	}
 
-  initActions() {
-    this.setActionDefinitions({
-      multiviewerlayout: {
-        name: 'Change multiviewer layout',
-        options: [
-          {
-            type: 'dropdown',
-            label: 'Layout number',
-            id: 'layoutNumber',
-            default: '0',
-            choices: [
-              { id: '0', label: '1' },
-              { id: '1', label: '2' },
-              { id: '2', label: '3' },
-              { id: '3', label: '4' },
-            ],
-          },
-        ],
-        callback: async (event) => {
-          const cmd = `<System id="0" GUID="542696d038d3-bcc201"><FrameCollection id="0"><Frame id="${this.config.macAddress}"><MultiViewer id="0"><LayoutSelect>${event.options.layoutNumber}</LayoutSelect></MultiViewer></Frame></FrameCollection></System>`
-          this.log('info', `Sending multiviewer layout: ${event.options.layoutNumber}`)
-          this.sendCommand(cmd)
-        },
-      },
+	initActions() {
+		this.setActionDefinitions({
+			multiviewerlayout: {
+				name: 'Change multiviewer layout',
+				options: [
+					{
+						type: 'dropdown',
+						label: 'Layout number',
+						id: 'layoutNumber',
+						default: '0',
+						choices: [
+							{ id: '0', label: '1' },
+							{ id: '1', label: '2' },
+							{ id: '2', label: '3' },
+							{ id: '3', label: '4' },
+						],
+					},
+				],
+				callback: async (event) => {
+					const cmd = `<System id="0" GUID="542696d038d3-bcc201"><FrameCollection id="0"><Frame id="${this.config.macAddress}"><MultiViewer id="0"><LayoutSelect>${event.options.layoutNumber}</LayoutSelect></MultiViewer></Frame></FrameCollection></System>`
+					this.log('info', `Sending multiviewer layout: ${event.options.layoutNumber}`)
+					this.sendCommand(cmd)
+				},
+			},
+			
+			audioTone: {
+				name: 'Set audio tone on output',
+				options: [
+					{
+						type: 'dropdown',
+						label: 'Destination',
+						id: 'destination',
+						default: '0',
+						choices: [
+							{ id: '0', label: 'PGM 1A' },
+							{ id: '1', label: 'PGM 1B' },
+							{ id: '2', label: 'PGM 2A' },
+							{ id: '3', label: 'PGM 2B' },
+						],
+					},
+					{
+						type: 'dropdown',
+						label: 'on/off',
+						id: 'onOff',
+						default: '0',
+						choices: [
+							{ id: '0', label: 'off' },
+							{ id: '1', label: 'on' },
+						],
+					},
+				],
+				callback: async (event) => {
+					const cmd = `<System id="0" GUID="a0afbd8052e0-003b54" OPID="-1"><DestMgr id="0"><ScreenDestCol id="0"><ScreenDest id=${event.options.destination}><AudioTestTone>${event.options.onOff}</AudioTestTone></ScreenDest></ScreenDestCol></DestMgr></System>`
+					this.log(
+						'info',
+						`Sending audio tone command: ${event.options.onOff} to destination: ${event.options.destination}`,
+					)
+					this.sendCommand(cmd)
+				},
+			},
 
-      userkeys: {
-        name: 'Apply userkey',
-        options: [
-          {
-            type: 'textinput',
-            label: 'Userkey number (sending -1)',
-            id: 'userKey',
-            default: '1',
-          },
-          {
-            type: 'textinput',
-            label: 'Screen Destination (sending -1)',
-            id: 'screenDest',
-            default: '1',
-          },
-          {
-            type: 'textinput',
-            label: 'Layer (sending -1)',
-            id: 'layer',
-            default: '1',
-          },
-        ],
-        callback: async (event) => {
-          const screenDest = parseInt(event.options.screenDest) - 1
-          const userKey = parseInt(event.options.userKey) - 1
-          const layer = parseInt(event.options.layer) - 1
+			userkeys: {
+				name: 'Apply userkey',
+				options: [
+					{
+						type: 'textinput',
+						label: 'Userkey number (sending -1)',
+						id: 'userKey',
+						default: '1',
+					},
+					{
+						type: 'textinput',
+						label: 'Screen Destination (sending -1)',
+						id: 'screenDest',
+						default: '1',
+					},
+					{
+						type: 'textinput',
+						label: 'Layer (sending -1)',
+						id: 'layer',
+						default: '1',
+					},
+				],
+				callback: async (event) => {
+					const screenDest = parseInt(event.options.screenDest) - 1
+					const userKey = parseInt(event.options.userKey) - 1
+					const layer = parseInt(event.options.layer) - 1
 
-          const cmd = `<System id="0" GUID="005056c00001-00617e"><DestMgr id="0"><ScreenDestCol id="0"><ScreenDest id="${screenDest}"><LayerCollection id="0"><Layer id="${layer}"><LastUserKeyIdx>${userKey}</LastUserKeyIdx><ApplyUserKey>${userKey}</ApplyUserKey></Layer></LayerCollection></ScreenDest></ScreenDestCol></DestMgr></System>`
-          this.sendCommand(cmd)
-        },
-      },
+					const cmd = `<System id="0" GUID="005056c00001-00617e"><DestMgr id="0"><ScreenDestCol id="0"><ScreenDest id="${screenDest}"><LayerCollection id="0"><Layer id="${layer}"><LastUserKeyIdx>${userKey}</LastUserKeyIdx><ApplyUserKey>${userKey}</ApplyUserKey></Layer></LayerCollection></ScreenDest></ScreenDestCol></DestMgr></System>`
+					this.sendCommand(cmd)
+				},
+			},
 
-      custom: {
-        name: 'Custom XML',
-        options: [
-          {
-            type: 'textinput',
-            label: 'XML content',
-            id: 'custom',
-            default: '',
-          },
-        ],
-        callback: async (event) => {
-          this.sendCommand(event.options.custom)
-        },
-      },
-    })
-  }
+			custom: {
+				name: 'Custom XML',
+				options: [
+					{
+						type: 'textinput',
+						label: 'XML content',
+						id: 'custom',
+						default: '',
+					},
+				],
+				callback: async (event) => {
+					this.sendCommand(event.options.custom)
+				},
+			},
+		})
+	}
 
-  sendCommand(cmd) {
-    if (this.tcp && cmd) {
-      this.log('debug', `Sending: ${cmd}`)
-      this.tcp.send(cmd)
-    }
-  }
+	sendCommand(cmd) {
+		if (this.tcp && cmd) {
+			this.log('debug', `Sending: ${cmd}`)
+			this.tcp.send(cmd)
+		}
+	}
 }
 
 runEntrypoint(EventmasterXmlInstance)
